1edcf8e2441ab65a8c60a0260c13c775a4b2b117
Laszlo Farkas <avatarf@gmail.com>
2022-11-08 17:00:05 +0100

    Init flask test

---
+++ helloworld/main.py
@@ -0,0 +1,8 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+    return "<h1>Hello world</h1>"
---
+++ helloworld/wsgi.py
@@ -0,0 +1,4 @@
+from helloworld.main import app
+
+if __name__ == "__main__":
+    app.run()
